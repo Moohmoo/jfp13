a010982d4876900fcc861366bd63eef75195e1af
Mohamed Oussaren <91945901+Moohmoo@users.noreply.github.com>
2023-05-31 16:37:26 +0200

    Update exercice_4.py

--- exercice_4.py
+++ exercice_4.py
@@ -6,6 +6,28 @@ __date__ = "31/05/2023"
 __version__ = "0.0.1"
 __copyright__ = "CC BY-SA"
 
+import sys
+import random
 
-def exercice_4():
-    pass
+def exercice_4(k, G):
+    for _ in range(k):
+        pos_gen1, pos_gen2 = 0, 0
+        score = 0
+        rondes = 100
+        for i in range(rondes) :
+            print(i, file=sys.stderr)
+            if i == 0:
+                corespondance = sorted(G.degree, key=lambda x: x[1], reverse=True)[0:50]
+                pos_gen1 = str(random.choice(corespondance)[0])
+                pos_gen2 = str(random.choice(corespondance)[1])
+                print(pos_gen1, pos_gen2, file=sys.stderr)
+            else :
+                pos_gen1 = str(list(G.neighbors(pos_gen1))[0])
+                pos_gen2 = str(list(G.neighbors(pos_gen2))[0])
+            print(pos_gen1 + " " + pos_gen2, flush=True)
+            v = input()
+            if v == -1:
+                score = (101 - rondes) * 10
+            elif v == -2 and i == rondes - 1:
+                score = 0
+            # print(i, file=sys.stderr)
